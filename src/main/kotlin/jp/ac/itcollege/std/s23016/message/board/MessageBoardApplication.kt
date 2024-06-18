e0af9f8e5c4b0258449019f597d09c56a651a33e
s23016 <s23016@std.it-college.ac.jp>
2024-06-18 14:19:52 +0900

    初期設定

---
+++ src/main/kotlin/jp/ac/itcollege/std/s23016/message/board/MessageBoardApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.itcollege.std.s23016.message.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessageBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<MessageBoardApplication>(*args)
+}
---
+++ src/test/kotlin/jp/ac/itcollege/std/s23016/message/board/MessageBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.itcollege.std.s23016.message.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessageBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
